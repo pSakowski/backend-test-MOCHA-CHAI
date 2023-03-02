ef694d97a239566a708c3c97602e03d240a75edd
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-02 21:05:07 +0100

    test content without changes if proper args

--- utils/cutText.js
+++ utils/cutText.js
@@ -1,7 +1,6 @@
 module.exports = (content, maxLength) => {
   if (!content || typeof content !== 'string') return 'Error';
-  if (typeof maxLength !== 'number') return 'Error';
-  if (typeof maxLength <= 0 || -6) return 'Error';
+  if (typeof maxLength !== 'number' || maxLength <= 0) return 'Error';
   if (content.length <= maxLength) return content;
   return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
 };
