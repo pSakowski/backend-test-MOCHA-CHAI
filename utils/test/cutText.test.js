a97cee2c8c619eb658f8d12e885d6250b3f8bf8b
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-02 20:51:15 +0100

    test if content arg is not a string

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -3,12 +3,16 @@ const expect = require('chai').expect;
 
 describe('CutText', () => {
 
-    it('should return an error if "content" arg is not a string', () => {
-        expect(cutText(undefined, 20)).to.equal('Error');
-        expect(cutText(12, 20)).to.equal('Error');
-        expect(cutText({}, 20)).to.equal('Error');
-        expect(cutText([], 20)).to.equal('Error');
-        expect(cutText(function() {}, 20)).to.equal('Error');
-      });
-  
+  it('should return an error if "content" arg is not a string', () => {
+    expect(cutText(undefined, 20)).to.equal('Error');
+    expect(cutText(12, 20)).to.equal('Error');
+    expect(cutText({}, 20)).to.equal('Error');
+    expect(cutText([], 20)).to.equal('Error');
+    expect(cutText(function () { }, 20)).to.equal('Error');
   });
+
+  it('should return an error if "content" arg length is 0', () => {
+    expect(cutText('', 20)).to.equal('Error');
+  });
+
+});
